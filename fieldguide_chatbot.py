f6c683b087f1915235a106d33c3f6cbf0fad6d7f
Declan McKenna <deco354@gmail.com>
2023-08-09 10:49:39 +0100

    Wrap long strings

--- fieldguide_chatbot.py
+++ fieldguide_chatbot.py
@@ -48,27 +48,35 @@ def get_response_from_query(database, query, vector_count=8):
     
     gpt = ChatOpenAI(model_name="gpt-3.5-turbo-16k")
     system_template = """
-        You are an intelligent chatbot assistant designed to answer questions about automattic's fieldguide documents
-        based on excerpts of these documents within triple backticks below.
+        You are an intelligent chatbot assistant designed to answer questions about 
+        automattic's fieldguide documents based on excerpts of these documents 
+        within triple backticks below.
         ```
         {doc_content}
         ```
         Context:
-        - Automattic is a US based software development company responsible for products such as
-        Wordpress.com and Tumblr.
-        - Automattic's "fieldguide" is an employee manual used to help employees navigate
-        working at the company
-        - Automattic is a fully remote company, making this field guide a very important reference point.
+        - Automattic is a US based software development company responsible for 
+        products such as Wordpress.com and Tumblr.
+        - Automattic's "field guide" is an employee manual used to help employees 
+        navigate working at the company
+        - Automattic is a fully remote company, making this field guide a very 
+        important reference point.
 
         Instructions:
-        - Only use the factual information from the document excerpts to answer the question.
+        - Only use the factual information from the document excerpts to 
+        answer the question.
         - If you're unsure of an answer, you can say "I don't know" or "I'm not sure"
-        - Don't mention that you're using excerpts of the fields guide, this is an implementation detail the user doesn't need to know.
+        - Don't mention that you're using excerpts of the fields guide, this is an 
+        implementation detail the user doesn't need to know.
+        - If someone asks how to do something that is explained in the field guide 
+        provide as much details as you can from it.
         """
     
     system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)    
     
-    human_template = "Answer the following question that has been placed within angle brackets <{query}>"
+    human_template = """
+    Answer the following question that has been placed within angle brackets <{query}>
+    """
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
     
     chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
@@ -83,12 +91,12 @@ text_files = get_textfiles_from_directory("Fieldguide Docs")
     
 database = create_database_from_text_strings(text_files)
 travel_upgrade_question = """
-I'm going to a conference to meet my colleagues at Tumblr, I'll be flying from the UK to the US. 
-What travel upgrades can I purchase?
+I'm going to a conference to meet my colleagues at Tumblr, 
+I'll be flying from the UK to the US. What travel upgrades can I purchase?
 """
 personal_travel_question= """
-I would like to arrive to the conference 3 days early so I can visit San Francisco over the weekend. 
-Am I allowed to expense flights for this?
+I would like to arrive to the conference 3 days early so I can visit San Francisco 
+over the weekend. Am I allowed to expense my flight if I book it 3 days earlier?
 """
 response = get_response_from_query(database, personal_travel_question)
 pretty_print(response)
